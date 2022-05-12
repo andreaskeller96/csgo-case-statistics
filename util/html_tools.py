0b872a73746f0222c53c1f935ff5aeaf1f0e1e09
Andreas Keller <andi.keller@tum.de>
2022-05-12 13:32:35 +0200

    json variables

--- util/html_tools.py
+++ util/html_tools.py
@@ -1,4 +1,5 @@
 import ast
+import json
 
 def get_variable_from_html(var_name, htm_string):
     start = htm_string.find(var_name)
@@ -10,3 +11,16 @@ def get_variable_from_html(var_name, htm_string):
         return []
     var_obj = ast.literal_eval(var_string)
     return var_obj
+
+def get_json_variable_from_html(var_name, htm_string):
+    start = htm_string.find(var_name)
+    var_string = htm_string[start:].split("\n")[0]
+    var_string = var_string.split("=")[1]
+    var_string = var_string.lstrip()
+    if var_string[-1]==";":
+        var_string = var_string[:-1]
+    elif var_string[-2] == ";":
+        var_string = var_string[:-2]
+    if var_string == "null":
+        return {}
+    return json.loads(var_string)
